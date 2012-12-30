bde909d10a8420a33f420a3af02033a633c403ee
Sushant <forksushant@gmail.com>
2012-12-30 08:39:46 +0530

    chapter 3 finished

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   describe "Home page" do
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -11,7 +12,7 @@ describe "StaticPages" do
     it "should have the title 'Home'" do
     	visit '/static_pages/home'
     	page.should have_selector('title',
-    						:text => "Ruby on Rails Tutorial Sample App | Home")
+    						:text => "#{base_title} | Home")
     end
   end
   describe "Help Page" do
@@ -23,7 +24,7 @@ describe "StaticPages" do
   	it "should have the title 'Help'" do
     	visit '/static_pages/help'
     	page.should have_selector('title',
-    						:text => "Ruby on Rails Tutorial Sample App | Help")
+    						:text => "#{base_title} | Help")
     end
   end
   describe "About page" do
@@ -34,8 +35,18 @@ describe "StaticPages" do
 	  it "should have the title 'About Us'" do
     	visit '/static_pages/about'
     	page.should have_selector('title',
-    						:text => "Ruby on Rails Tutorial Sample App | About Us")
-    end
+    						:text => "#{base_title} | About Us")
+    	end
+	end
+	describe "Contact Us page" do
+		it "should have the content 'Contact Us'" do
+			visit '/static_pages/contact'
+			page.should have_selector("h1", :text => 'Contact Us')		
+		end
+		it "should have the title 'Contact Us'" do
+			visit '/static_pages/contact'			
+			page.should have_selector('title', :text => "#{base_title} | Contact Us")
+		end
 	end
 
 end
